5e035977a3c0b06eb63598a016a5f9a17a867e57
Amal K Reji <amalreji45.amal@gmail.com>
2024-10-01 15:22:26 +0530

    added admin parts

--- controllers/admin-helper.js
+++ controllers/admin-helper.js
@@ -10,121 +10,199 @@ module.exports = {
             try {
                 const result = await db.getdb().collection(collections.CATEGORY_COLLECTION).insertOne(category);
                 console.log('Category Added');
-                resolve(result); // Resolve with the result of the insertion
+                resolve(result);
+                if (result.acknowledged) {
+                    const activity = {
+                        action: 'New Category Added: ' + category.name,
+                        user: 'Admin',
+                        status: 'Added',
+                        date: new Date()
+                    };
+                    db.getdb().collection(collections.ACTIVITY_COLLECTION).insertOne(activity);
+                    console.log('Category Updated');
+
+                }
             } catch (err) {
                 console.error('Error adding category:', err);
-                reject(err); // Reject the promise with the error
+                reject(err);
             }
         });
     },
 
-    addRecipe(data){
-        return new Promise(async(resolve,reject)=>{
-            const recipe = await db.getdb().collection(collections.RECIPE_COLLECTION).insertOne(data).then((result)=>{
+    addRecipe(data) {
+        return new Promise(async (resolve, reject) => {
+            const recipe = await db.getdb().collection(collections.RECIPE_COLLECTION).insertOne(data).then((result) => {
                 console.log('Recipe Added');
-                resole(result);
+                resolve(result);
+                if (result.acknowledged) {
+                    const activity = {
+                        action: 'New Recipe Added: ' + data.name,
+                        user: 'Admin',
+                        status: 'Added',
+                        date: new Date()
+                    };
+                    db.getdb().collection(collections.ACTIVITY_COLLECTION).insertOne(activity);
+                    console.log('Category Updated');
+
+                };
             })
 
         })
     },
 
-    getTotalRecipes(){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.RECIPE_COLLECTION).find().count().then((total)=>{
+    getTotalRecipes() {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.RECIPE_COLLECTION).find().count().then((total) => {
                 resolve(total);
             });
-            
+
         })
     },
 
-    getTotalCategory(){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.CATEGORY_COLLECTION).find().count().then((total)=>{
+    getTotalCategory() {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.CATEGORY_COLLECTION).find().count().then((total) => {
                 resolve(total);
             })
         })
     },
 
-    getRecentActivites(){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.ACTIVITY_COLLECTION).find().toArray().then((activity)=>{
+    getRecentActivites() {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.ACTIVITY_COLLECTION).find().toArray().then((activity) => {
                 resolve(activity);
             })
         })
     },
 
-    getCategory(){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.CATEGORY_COLLECTION).find().toArray().then((category)=>{
+    getCategory() {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.CATEGORY_COLLECTION).find().toArray().then((category) => {
                 resolve(category);
             })
         })
     },
 
-    deleteCategory(categoryId){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.CATEGORY_COLLECTION).deleteOne({_id:new objectId(categoryId)}).then((result)=>{
+    deleteCategory(categoryId) {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.CATEGORY_COLLECTION).deleteOne({ _id: new objectId(categoryId) }).then((result) => {
                 console.log(result);
                 resolve(result);
-                if(result.acknowledged){
+                if (result.acknowledged) {
                     const activity = {
-                        action :'Deleted Category',
-                        user :'Admin',
-                        status :'Deleted',
-                        date :new Date()
+                        action: 'Deleted Category',
+                        user: 'Admin',
+                        status: 'Deleted',
+                        date: new Date()
                     };
                     db.getdb().collection(collections.ACTIVITY_COLLECTION).insertOne(activity);
                     console.log('Category Updated');
-                    
+
                 }
             })
         })
-        
+
     },
 
-    getAllRecipes(){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.RECIPE_COLLECTION).find().toArray().then((recipe)=>{
+    getAllRecipes() {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.RECIPE_COLLECTION).find().toArray().then((recipe) => {
                 //console.log(recipe);
                 resolve(recipe);
             })
         })
     },
 
-    getCategoryDetails(categoryId){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.CATEGORY_COLLECTION).findOne({_id:new objectId(categoryId)}).then((category)=>{               
+    getCategoryDetails(categoryId) {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.CATEGORY_COLLECTION).findOne({ _id: new objectId(categoryId) }).then((category) => {
                 resolve(category);
             })
         })
     },
 
-    updateCategory(categoryId,categoryDetails){
-        return new Promise(async(resolve,reject)=>{
-            await db.getdb().collection(collections.CATEGORY_COLLECTION).updateOne({_id:new objectId(categoryId)},{
-                $set:{
-                    name:categoryDetails.name,
-                    image:categoryDetails.image
+    updateCategory(categoryId, categoryDetails) {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.CATEGORY_COLLECTION).updateOne({ _id: new objectId(categoryId) }, {
+                $set: {
+                    name: categoryDetails.name,
+                    image: categoryDetails.image
                 }
-            }).then((result)=>{
+            }).then((result) => {
                 resolve(result);
-                if(result.acknowledged){
+                if (result.acknowledged) {
                     const activity = {
-                        action :'Updated Category',
-                        user :'Admin',
-                        status :'Completed',
-                        date :new Date()
+                        action: 'Updated Category',
+                        user: 'Admin',
+                        status: 'Completed',
+                        date: new Date()
                     };
                     db.getdb().collection(collections.ACTIVITY_COLLECTION).insertOne(activity);
                     console.log('Category Updated');
-                    
+
                 }
             })
-            
+
         })
 
-    }
-    
+    },
+
+    deleteRecipe(recipeId) {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.RECIPE_COLLECTION).deleteOne({ _id: new objectId(recipeId) }).then((result) => {
+                resolve(result);
+                if (result.acknowledged) {
+                    const activity = {
+                        action: 'Deleted Recipe',
+                        user: 'Admin',
+                        status: 'Completed',
+                        date: new Date()
+                    };
+                    db.getdb().collection(collections.ACTIVITY_COLLECTION).insertOne(activity);
+                    console.log('Category Updated');
+
+                }
+            });
+        });
+    },
+
+    getRecipeDetails(recipeId) {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.RECIPE_COLLECTION).findOne({ _id: new objectId(recipeId) }).then((recipe) => {
+                resolve(recipe);
+            })
+        })
+    },
+
+    updateRecipe(recipeId, recipeDetails) {
+        return new Promise(async (resolve, reject) => {
+            await db.getdb().collection(collections.RECIPE_COLLECTION).updateOne({ _id: new objectId(recipeId) }, {
+                $set: {
+                    name: recipeDetails.name,
+                    description: recipeDetails.description,
+                    ingredients: recipeDetails.ingredients,
+                    making: recipeDetails.steps,
+                    email: recipeDetails.email,
+                    category: recipeDetails.category
+                }
+            }).then((result) => {
+                resolve(result);
+                // console.log(result);
+                if (result.acknowledged) {
+                    const activity = {
+                        action: 'Recipe Updated :' + recipeDetails.name,
+                        user: 'Admin',
+                        status: 'Completed',
+                        date: new Date()
+                    };
+                    db.getdb().collection(collections.ACTIVITY_COLLECTION).insertOne(activity);
+                    console.log('Category Updated');
+
+                }
+
+            });
+        });
+    },
 
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -11,20 +11,23 @@ router.get('/', function(req, res, next) {
     adminHelper.getRecentActivites()
   ])
   .then(([recipeCount,categoryCount,activity])=>{
-    const total = {recipeCount,categoryCount,activity};
+    const limitActivity = activity.reverse().slice(0,5);
+
+    const total = {recipeCount,categoryCount,limitActivity};
     res.render('admin/admin-home',{admin:true,total});
   });
 
 });
 
 router.get('/add-category',(req,res)=>{
-  res.render('admin/add-category');
+  res.render('admin/add-category',{success:req.flash('success')});
 });
 
 router.post('/add-category',(req,res)=>{
   console.log(req.body);
   adminHelper.addCategory(req.body).then((result)=>{
     console.log(result);
+    req.flash('success','New Category Added')
     res.redirect('/admin/add-category');
   })
   .catch((err)=>{
@@ -33,7 +36,7 @@ router.post('/add-category',(req,res)=>{
 });
 
 router.get('/add-recipe',(req,res)=>{
-  res.render('admin/add-recipe');
+  res.render('admin/add-recipe',{success:req.flash('success')});
 });
 
 router.post('/add-recipe',(req,res)=>{
@@ -45,6 +48,7 @@ router.post('/add-recipe',(req,res)=>{
     console.log(id);
     image.mv('./public/uploads/recipes/'+imageName+'.png',((err,done)=>{
       if(!err){
+        req.flash('success','Recipe added successfully');
         res.redirect('/admin/add-recipe');
       }else{
         console.log(err);
@@ -56,22 +60,23 @@ router.post('/add-recipe',(req,res)=>{
 
 router.get('/all-recipes',(req,res)=>{
   adminHelper.getAllRecipes().then((recipe)=>{
-    res.render('admin/all-recipe',{recipe});
-  })
-  
+    const deleteMessage = req.flash('deleteUpdate')[0] || null;
+    const updateMessage = req.flash('successUpdate')[0] || null;
 
+    res.render('admin/all-recipe',{recipe,deleteMessage,updateMessage});
+  })
 });
 
 router.get('/all-category',(req,res)=>{
   adminHelper.getCategory().then((category)=>{
-    res.render('admin/view-category',{category});
+    res.render('admin/view-category',{category,deleteError:req.flash('deleteError')});
   })
-  
 });
 
 router.get('/delete-category/:id',(req,res)=>{
   let categoryId = req.params.id;
   adminHelper.deleteCategory(categoryId).then((result)=>{
+    req.flash('deleteError','Category deleted successfully');
     res.redirect('/admin/all-category');
   })
 });
@@ -91,6 +96,36 @@ router.post('/edit-category/:id',(req,res)=>{
     res.redirect('/admin/all-category');
     
   })
+});
+
+router.get('/delete-recipe/:id',(req,res)=>{
+  let recipeId = req.params.id;
+  adminHelper.deleteRecipe(recipeId).then((result)=>{
+    req.flash('deleteUpdate','Recipe deleted successfully');
+    res.redirect('/admin/all-recipes');
+  })
+});
+
+router.get('/edit-recipe/:id',(req,res)=>{
+  let recipeId = req.params.id;
+  adminHelper.getRecipeDetails(recipeId).then((recipe)=>{
+    console.log(recipe);
+    res.render('admin/edit-recipe',{recipe});
+  })
+});
+
+router.post('/update-recipe/:id',(req,res)=>{
+  let recipeId = req.params.id;
+  adminHelper.updateRecipe(recipeId,req.body).then((result)=>{
+    req.flash('successUpdate','Recipe updated successfully');
+    res.redirect('/admin/all-recipes');
+    if(req.files.image){
+      let image = req.files.image;
+      let imageName = req.body.name;
+      image.mv('./public/uploads/recipes/'+imageName+'.png');
+    }
+  })
+
 })
 
 
